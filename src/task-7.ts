55705d5fff2e294d4b124db762f79774b317c71e
Tatiana <petrenkotanya18@gmail.com>
2025-05-25 22:49:38 +0300

    completed practical tasks

--- src/task-7.ts
+++ src/task-7.ts
@@ -1,5 +1,5 @@
 function getMessage() {
-  return new Promise((resolve) => {
+  return new Promise<string>((resolve) => {
     setTimeout(() => {
       resolve("Hello from TS");
     }, 1000);
--- src/task-8.ts
+++ src/task-8.ts
@@ -1,12 +1,17 @@
-// import axios from "axios";
+import axios from "axios";
 
-// async function fetchPosts() {
-//   const response = await axios.get(
-//     '<https://jsonplaceholder.typicode.com/posts>'
-//   );
-//   return response.data;
-// }
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return response.data;
+}
 
-// fetchPosts().then((posts) => {
-//   console.log(posts[0].title);
-// });
+fetchPosts().then((posts) => {
+  console.log(posts[0].title);
+});
